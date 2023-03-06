112e7f7720dd7e63dc4503680f012b80e6ad2074
Mateus Barros <mateusbarros2000@gmail.com>
2023-03-06 13:04:13 -0300

    criação interface da Netflix

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = 'e787a5924955ce25993f404c9336f5ed'
+
+let img_url = 'https://image.tmdb.org/t/p/w500'
+
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+
+let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
